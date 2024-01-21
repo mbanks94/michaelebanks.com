71e4b9258e9d2b9ed07bae12526efb1b4d806108
Michael Banks <93537279+mbanks632@users.noreply.github.com>
2024-01-20 23:29:26 -0600

    render repo data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { useQuery } from '@/lib/hooks';
+import { GetRepositories, RepoData, RepoVariables } from '@/lib/queries';
+
 export default function Page() {
+  const { loading, error, data } = useQuery<RepoData, RepoVariables>(
+    GetRepositories,
+    {
+      variables: { username: 'mbanks94' },
+    }
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error:</p>;
+
   return (
     <section>
+      <h1 className='mb-8'>Projects</h1>
       <div>
-        <h1>Projects</h1>
+        {data?.user.repositories.nodes.map((repo) => {
+          return (
+            <>
+              <p>{repo.name}</p>
+              <p>{repo.description}</p>
+            </>
+          );
+        })}
       </div>
     </section>
   );
