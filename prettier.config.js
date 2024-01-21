9f8c6a598866b28e42d5acf6c517caf146c45b9c
Michael Banks <93537279+mbanks632@users.noreply.github.com>
2024-01-20 18:56:48 -0600

    more eslint prettier conf

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   singleQuote: true,
   jsxSingleQuote: true,
-  trailingComma:  "es5",
+  trailingComma: 'es5',
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['^components/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
