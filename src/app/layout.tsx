e582bfd2b294fe51bfe1e5aa9d200cbd2e52069e
Michael Banks <93537279+mbanks632@users.noreply.github.com>
2024-01-20 19:49:50 -0600

    update layout and globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import { cx } from '@/lib/utils';
 
 import './globals.css';
 
@@ -25,7 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className={cx(
+        'text-black bg-white dark:text-white dark:bg-[#212121]',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
       <body className={inter.className}>{children}</body>
     </html>
   );
